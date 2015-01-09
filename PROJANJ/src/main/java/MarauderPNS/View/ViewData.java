fec27646baf779b858a9a02bc0393b7f190f01ae
Quentin CORNEVIN <cornevin@polytech.unice.fr>
2015-01-09 16:58:27 +0100

    SIJANJ-44

---
+++ PROJANJ/src/main/java/MarauderPNS/View/ViewData.java
@@ -0,0 +1,9 @@
+package MarauderPNS.view;
+
+/**
+ * Created by Quentin on 09/01/2015.
+ */
+public interface ViewData {
+
+    String title = "Carte du Marauder";
+}
---
+++ PROJANJ/src/main/java/MarauderPNS/simulation/Launcher.java
@@ -0,0 +1,13 @@
+package MarauderPNS.simulation;
+
+import MarauderPNS.view.GridView;
+
+/**
+ * Created by Quentin on 09/01/2015.
+ */
+public class Launcher {
+
+    public static void main( String[] args ) {
+        GridView grid = new GridView(20,20);
+    }
+}
