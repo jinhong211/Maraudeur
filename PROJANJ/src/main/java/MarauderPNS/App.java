5c584d77fb8324b1dd28bb58502263d6de78e925
Garance <vallat.garance@etu.unice.fr>
2015-01-09 15:06:49 +0100

    test branche client

--- PROJANJ/src/main/java/MarauderPNS/App.java
+++ PROJANJ/src/main/java/MarauderPNS/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Ca marche !");
+        System.out.println("Je suis maintenant sur la branch Client");
 
     }
 }
