1218dc62e809dacd28ad440737d29c5b325160e2
Garance <vallat.garance@etu.unice.fr>
2015-01-12 12:56:11 +0100

    JSON communication to get the users of a new simulation + https certificate as an image SIJANJ-59 SIJANJ-54

--- PROJANJ/src/main/java/MarauderPNS/communication/Client.java
+++ PROJANJ/src/main/java/MarauderPNS/communication/Client.java
@@ -1,10 +1,18 @@
 package MarauderPNS.communication;
 
 
+import MarauderPNS.user.Student;
+import MarauderPNS.user.Teacher;
+import MarauderPNS.user.User;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import javax.net.ssl.HttpsURLConnection;
 /**
  * The client class dedicated to communicating with the server, via the https protocol for now
@@ -15,39 +23,112 @@ import javax.net.ssl.HttpsURLConnection;
  * that both are using the same key, and the connection is closed. In order to host https connections, a server must have
  * a public key certificate, which embeds key information with a verification of the key owner's identity.
  * Most certificates are verified by a third party so that clients are assured that the key is secure.
+ * Here however, our server has an auto signed certificate, so we must make sure that we still accept it.
  */
 
 public class Client
 {
 	private JSONGenerator jSONGenerator;
+    private PrintWriter out;
+    private BufferedReader in;
 
+    /**
+     * Client constructor. All the code is useless for now,
+     * kept as a model for when we'll have to write stuff to the server, or get stuff from him with a real JSOn request
+     */
 	public Client() {
-        try {
+        jSONGenerator = new JSONGenerator();
+
+      /*  try {
+
             //Here is my URL
             String httpsURL = "https://www.gmail.com/";
             URL myurl = new URL(httpsURL);
-            //Let's connect together !
+            //WARNING : here, it is likely to crash because the certificate is self-signed
             HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection();
             //In a few steps, let's get what's on the server
             InputStream ins = con.getInputStream();
             InputStreamReader isr = new InputStreamReader(ins);
-            BufferedReader in = new BufferedReader(isr);
+            in = new BufferedReader(isr);
             String inputLine;
             //what's on the server now ??
             while ((inputLine = in.readLine()) != null) {
                 System.out.println(inputLine); //this will be what's in the JSON format
             }
+            //And if I want to send stg to the server ?
+            OutputStream outs = con.getOutputStream();
+            out = new PrintWriter(outs, true);
+            out.println("That's for the server !");
             in.close();
-
-
             //This is how to get data from the server. Now, to send him some info :
         }
         catch (Exception e) {
             System.err.println("Exception !");
             e.printStackTrace();
         }
+        */
 
 	}
+
+
+    //Besoin de se connecter ICI : maraudeur.neowutran.net/start_simulation
+    //Valable seulement poru le 1er sprint, empêche de personnaliser al simulation...
+    //Et pas de requête en JSON
+    public HashMap<Integer, User> beginSimulation() {
+        String iGet = "";
+        try {
+            String httpsURL = "maraudeur.neowutran.net/start_simulation";
+            URL myurl = new URL(httpsURL);
+            //WARNING : here, it is likely to crash because the certificate is self-signed
+            HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection();
+            //In a few steps, let's get what's on the server
+            InputStream ins = con.getInputStream();
+            InputStreamReader isr = new InputStreamReader(ins);
+            in = new BufferedReader(isr);
+            String inputLine;
+            //what's on the server now ??
+            while ((inputLine = in.readLine()) != null) {
+                iGet = (inputLine); //this will be what's in the JSON format
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+            System.err.println("Impossible de lire la réponse du serveur");
+            iGet =  "{\"return\":" +
+                    "[" +
+                    "{\"user\":{\"id\":62,\"status\":\"Teacher\"}}," +
+                    "{\"user\":{\"id\":63,\"status\":\" Student \"}}," +
+                    "{\"user\":{\"id\":64,\"status\":\"Student\"}}," +
+                    "{\"user\":{\"id\":65,\"status\":\"Student\"}}," +
+                    "{\"user\":{\"id\":66,\"status\":\"Teacher\"}}," +
+                    "{\"user\":{\"id\":67,\"status\":\"Teacher\"}}," +
+                    "{\"user\":{\"id\":68,\"status\":\"Student\"}}," +
+                    "{\"user\":{\"id\":69,\"status\":\"Teacher\"}}," +
+                    "{\"user\":{\"id\":70,\"status\":\"Student\"}}," +
+                    "{\"user\":{\"id\":71,\"status\":\"Teacher\"}}" +
+                    "]" +
+                    "}";
+
+        }
+        HashMap<Integer, String> listOfUsers = jSONGenerator.getNewSimulation(iGet);
+        return createUsers(listOfUsers);
+    }
+
+    /**
+     * Here, we already have a map with the id as key, plus a string which represents the status.
+     * @param listOfUsers
+     * @return the actual hashmap containing users
+     */
+    private HashMap<Integer, User> createUsers(HashMap<Integer, String> listOfUsers) {
+        HashMap<Integer, User> myUsers = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : listOfUsers.entrySet()) {
+            if (entry.getValue().equals("student"))
+                myUsers.put(entry.getKey(), new Student());
+            else
+                myUsers.put(entry.getKey(), new Teacher());
+        }
+        return myUsers;
+    }
 
 }
 
--- PROJANJ/src/main/java/MarauderPNS/communication/JSONGenerator.java
+++ PROJANJ/src/main/java/MarauderPNS/communication/JSONGenerator.java
@@ -1,34 +1,69 @@
 package MarauderPNS.communication;
 
 
-import MarauderPNS.communication.Client;
 
-/**
- * <br>
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
 
-public class JSONGenerator
-{
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public Client client;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	public JSONGenerator(){
-		super();
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class JSONGenerator {
+    private JSONObject json;
+    private JSONParser parser;
+
+    public JSONGenerator(){
+		json = new JSONObject();
+        parser = new JSONParser();
 	}
 
+
+    /**
+     * The method to ask the server to start a new simulation, with parameters : useless for the first sprint
+     * @return the json string
+     */
+    public String askNewSimulation() {
+
+
+        return null;
+    }
+
+    /**
+     * The method to translate from JSON to an array the list of users I asked for, to begin the simulation
+     * @param iGet
+     * @return the array of String, containing users
+     */
+    public HashMap<Integer, String> getNewSimulation(String iGet) {
+        //Ici, je récupère une liste, où chaque case contient : {"user":{"id":62,"status":"Teacher"}}
+        //NON ! Besoin d'un JSONOBJECT ! Que je peux décoder au fur & à mesure. Utiliser plutôt JSONSIMPLE du coup ! (aussi google)
+        HashMap<Integer, String> theUsers = new HashMap<>();
+        try {
+            Object obj = parser.parse(iGet);
+            json = (JSONObject) obj;
+            //On obtient le tableau de users
+            JSONArray msg = (JSONArray) json.get("return");
+            //On crée un itérateur pour le parcour
+            Iterator<String> iterator = msg.iterator();
+            // ma string : donc je la transforme en onbjet JSON encore
+            //
+            // {"user":{"id":62,"status":"Teacher"}}
+            while (iterator.hasNext()) {
+                obj = parser.parse(iterator.next());
+                JSONObject theUser = (JSONObject)((JSONObject)obj).get("user");
+                int hisId = (Integer)theUser.get("id");
+                String hisStatus = (String)theUser.get("status");
+                theUsers.put(hisId, hisStatus);
+            }
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return theUsers;
+    }
 }
 
