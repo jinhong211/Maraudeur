74e58b15cd0e8145ca820041a3b6b50bcb733d85
Didier Martini <me@neowutran.net>
2015-01-13 18:13:02 +0100

    #SIJANJ-54 https

--- PROJANJ/src/main/java/MarauderPNS/App.java
+++ PROJANJ/src/main/java/MarauderPNS/App.java
@@ -21,93 +21,13 @@ import java.util.HashMap;
 public class App 
 {
 
-    private void testIt(){
-
-        String https_url = "https://maraudeur.neowutran.net/start_simulation";
-        URL url;
-        try {
-
-            url = new URL(https_url);
-            HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
-
-            //dumpl all cert info
-            print_https_cert(con);
-
-            //dump all the content
-            print_content(con);
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private void print_https_cert(HttpsURLConnection con){
-
-        if(con!=null){
-
-            try {
-
-                System.out.println("Response Code : " + con.getResponseCode());
-                System.out.println("Cipher Suite : " + con.getCipherSuite());
-                System.out.println("\n");
-
-                Certificate[] certs = con.getServerCertificates();
-                for(Certificate cert : certs){
-                    System.out.println("Cert Type : " + cert.getType());
-                    System.out.println("Cert Hash Code : " + cert.hashCode());
-                    System.out.println("Cert Public Key Algorithm : "
-                            + cert.getPublicKey().getAlgorithm());
-                    System.out.println("Cert Public Key Format : "
-                            + cert.getPublicKey().getFormat());
-                    System.out.println("\n");
-                }
-
-            } catch (SSLPeerUnverifiedException e) {
-                e.printStackTrace();
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-
-        }
-
-    }
-
-    private void print_content(HttpsURLConnection con){
-        if(con!=null){
-
-            try {
-
-                System.out.println("****** Content of the URL ********");
-                BufferedReader br =
-                        new BufferedReader(
-                                new InputStreamReader(con.getInputStream()));
-
-                String input;
-
-                while ((input = br.readLine()) != null){
-                    System.out.println(input);
-                }
-                br.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-    }
-
-
     public static void main( String[] args )
     {
 
         System.out.println( "Hello World!" );
         System.out.println("Ca marche !");
         System.out.println("Je suis maintenant sur la branch Client");
-        new App().testIt();
+       // new App().testIt();
         /*System.out.println(result.size());
         for (Integer id : result.keySet()) {
             client.saveAMove(id, result.get(id));
